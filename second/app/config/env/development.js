a66c90dfbb6d6870aacc49bab9b7e813c1df1ce7
Bhupendra Singh <bbhupendra007@gmail.com>
2018-08-15 13:51:19 +0530

    README instruction added

--- second/app/config/env/development.js
+++ second/app/config/env/development.js
@@ -4,20 +4,7 @@ const DS = "/",
     base_url = "http://localhost:" + PORT + DS;
 
 module.exports = {
-    API: {
-        site: '/api/'
-    },
+    API: { site: '/api/' },
     base_url: base_url,
-    debug_mongo: false,
-    DS: DS,
-    image_destination: 'uploads',
-    image_dstn_w_slash: "./uploads/",
-    listing: {
-        limit: 10
-    },
-    secret: new Buffer("w5f1w153@#f123asf@#%f3g2&^%JH#2fc#@%@32@23%@#@@@@^%f2f23f#@@#fg").toString('base64'),
-    /*for sending emails*/
-    server: {
-        PORT: PORT
-    }
+    server: { PORT: PORT }
 };
